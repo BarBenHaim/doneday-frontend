260d297b96595e29956844fd74289f90d118de9f
Ariella Melnikov <melnikov.ariella@gmail.com>
2024-07-17 18:46:29 +0300

    add BoardIndex- basic

--- src/pages/BoardIndex.jsx
+++ src/pages/BoardIndex.jsx
@@ -1,11 +1,45 @@
+import { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+
+import { addBoard, loadBoards, removeBoard } from '../store/actions/board.action'
+
+import { showSuccessMsg, showErrorMsg } from '../services/board'
+
 import { BoardList } from '../cmps/BoardList'
 import { boardService } from '../services/board/board.service.local'
+import { loadBoards } from '../store/actions/board.action'
 
 export function BoardIndex() {
+
+    const boards = useSelector(storeState => storeState.boardModule.boards)
+
+    useEffect(() => {
+        loadBoards()
+    }, [])
+
+    async function onRemoveBoard(boardId) {
+        try {
+            await removeBoard(boardId)
+            showSuccessMsg('board removed')            
+        } catch (err) {
+            showErrorMsg('Cannot remove board')
+        }
+    }
+
+    async function onAddBoard() {
+        const board = boardService.getEmptyBoard()
+        try {
+            const savedBoard = await addBoard(board)
+            showSuccessMsg(`board added (id: ${savedBoard._id})`)
+        } catch (err) {
+            showErrorMsg('Cannot add board')
+        }        
+    }
+
     return (
         <section className='board-index'>
             <h1>This is board index</h1>
-            <BoardList />
+            <BoardList boards={boards} />
         </section>
     )
 }
