d185e7d47f4cffc870e4cef84e81669dd82730fd
Ariella Melnikov <melnikov.ariella@gmail.com>
2024-07-17 18:46:09 +0300

    add board action and reducer

---
+++ src/store/reducers/board.reducer.js
@@ -0,0 +1,69 @@
+export const SET_BOARDS = 'SET_BOARDS'
+export const SET_BOARD = 'SET_BOARD'
+export const REMOVE_BOARD = 'REMOVE_BOARD'
+export const ADD_BOARD = 'ADD_BOARD'
+export const UPDATE_BOARD = 'UPDATE_BOARD'
+export const ADD_BOARD_MSG = 'ADD_BOARD_MSG'
+
+const initialState = {
+    boards: [],
+    board: null
+}
+
+export function boardReducer(state = initialState, action) {
+    var newState = state
+    var boards
+    switch (action.type) {
+        case SET_BOARDS:
+            newState = { ...state, boards: action.boards }
+            break
+        case SET_BOARD:
+            newState = { ...state, board: action.board }
+            break
+        case REMOVE_BOARD:
+            const lastRemovedBoard = state.boards.find(board => board._id === action.boardId)
+            boards = state.boards.filter(board => board._id !== action.boardId)
+            newState = { ...state, boards, lastRemovedBoard: lastRemovedBoard }
+            break
+        case ADD_BOARD:
+            newState = { ...state, boards: [...state.boards, action.board] }
+            break
+        case UPDATE_BOARD:
+            boards = state.boards.map(board => (board._id === action.board._id) ? action.board : board)
+            newState = { ...state, boards }
+            break
+        case ADD_BOARD_MSG:
+            newState = { ...state, board: { ...state.board, msgs: [...state.board.msgs || [], action.msg] } }
+            break
+        default:
+    }
+    return newState
+}
+
+// unitTestReducer()
+
+// function unitTestReducer() {
+//     var state = initialState
+//     const board1 = { _id: 'b101', vendor: 'board ' + parseInt(Math.random() * 10), msgs: [] }
+//     const board2 = { _id: 'b102', vendor: 'board ' + parseInt(Math.random() * 10), msgs: [] }
+
+//     state = carReducer(state, { type: SET_CARS, cars: [car1] })
+//     console.log('After SET_CARS:', state)
+
+//     state = carReducer(state, { type: ADD_CAR, car: car2 })
+//     console.log('After ADD_CAR:', state)
+
+//     state = carReducer(state, { type: UPDATE_CAR, car: { ...car2, vendor: 'Good' } })
+//     console.log('After UPDATE_CAR:', state)
+
+//     state = carReducer(state, { type: REMOVE_CAR, carId: car2._id })
+//     console.log('After REMOVE_CAR:', state)
+
+//     const msg = { id: 'm' + parseInt(Math.random() * 100), txt: 'Some msg' }
+//     state = carReducer(state, { type: ADD_CAR_MSG, carId: car1._id, msg })
+//     console.log('After ADD_CAR_MSG:', state)
+
+//     state = carReducer(state, { type: REMOVE_CAR, carId: car1._id })
+//     console.log('After REMOVE_CAR:', state)
+// }
+
