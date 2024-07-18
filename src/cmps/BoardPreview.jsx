c10a29c7126ca940079e726b000d6c9b517ff406
Ariella Melnikov <melnikov.ariella@gmail.com>
2024-07-18 08:35:53 +0300

    design board preview data structur

--- src/cmps/BoardPreview.jsx
+++ src/cmps/BoardPreview.jsx
@@ -1,16 +1,28 @@
 import { Link } from 'react-router-dom'
+import { Box } from 'monday-ui-react-core'
+import { Board  } from "monday-ui-react-core/icons";
+import 'monday-ui-react-core/dist/main.css'
 
-// import boardPreviewImg from '../assets/img/board-preview/board-preview-01.png'
+
+import boardPreviewImg from '../assets/img/board-preview/board-preview-01.png'
 
 export function BoardPreview({ board }) {
     return (
         <article className='board-preview'>
             <Link to={`/board/${board._id}`}>
-                {/* <img src={boardPreviewImg} alt='boardPreviewImg' /> */}
-                {board.title}
+            <div class="board-preview-img">
+            <img src={boardPreviewImg} alt='boardPreviewImg' />
+            </div>
+                <span class="board-preview-item-wrapper flex">
+                <Board iconSize={18} iconLabel="Expand list" />
+                <div class="board-preview-title" >{board.title}</div>
+                </span>
+                <div class="board-preview-navigation" >
                 <p>
                     work managment {'>'} Main workspace {'>'} {board.title}{' '}
                 </p>
+                </div>
+
             </Link>
         </article>
     )
