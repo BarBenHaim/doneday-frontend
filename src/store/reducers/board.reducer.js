c26fc67f7f76f86ea26e132488b0eb11a0ed559a
Nir Fakiro <fakironir@gmail.com>
2024-07-18 15:17:16 +0300

    Add filter by

--- src/store/reducers/board.reducer.js
+++ src/store/reducers/board.reducer.js
@@ -13,116 +13,135 @@ export const UPDATE_TASK = 'UPDATE_TASK'
 export const REMOVE_TASK = 'REMOVE_TASK'
 
 const initialState = {
-    boards: [],
-    board: null,
+  boards: [],
+  board: null,
+  filterBy: {},
 }
 
 export const boardReducer = (state = initialState, action) => {
-    let newState = state
-    let boards
-    switch (action.type) {
-        case SET_BOARDS:
-            return { ...state, boards: action.boards }
-        case SET_BOARD:
-            return { ...state, board: action.board }
-        case REMOVE_BOARD:
-            const lastRemovedBoard = state.boards.find(board => board._id === action.boardId)
-            boards = state.boards.filter(board => board._id !== action.boardId)
-            return { ...state, boards, lastRemovedBoard }
-        case ADD_BOARD:
-            return { ...state, boards: [...state.boards, action.board] }
-        case UPDATE_BOARD:
-            boards = state.boards.map(board => (board._id === action.board._id ? action.board : board))
-            return { ...state, boards }
-        case ADD_BOARD_MSG:
-            return { ...state, board: { ...state.board, msgs: [...(state.board.msgs || []), action.msg] } }
-        case ADD_GROUP:
-            boards = state.boards.map(board =>
-                board._id === action.payload.boardId
-                    ? {
-                          ...board,
-                          groups: [...board.groups, action.payload.group],
-                      }
-                    : board
-            )
-            return { ...state, boards }
-        case UPDATE_GROUP:
-            boards = state.boards.map(board =>
-                board._id === action.payload.boardId
-                    ? {
-                          ...board,
-                          groups: board.groups.map(group =>
-                              group._id === action.payload.groupId ? action.payload.group : group
-                          ),
-                      }
-                    : board
-            )
-            return { ...state, boards }
-        case REMOVE_GROUP:
-            boards = state.boards.map(board =>
-                board._id === action.payload.boardId
-                    ? {
-                          ...board,
-                          groups: board.groups.filter(group => group._id !== action.payload.groupId),
-                      }
-                    : board
-            )
-            return { ...state, boards }
+  let newState = state
+  let boards
+  switch (action.type) {
+    case SET_BOARDS:
+      return { ...state, boards: action.boards }
+    case SET_BOARD:
+      return { ...state, board: action.board }
+    case REMOVE_BOARD:
+      const lastRemovedBoard = state.boards.find(
+        (board) => board._id === action.boardId
+      )
+      boards = state.boards.filter((board) => board._id !== action.boardId)
+      return { ...state, boards, lastRemovedBoard }
+    case ADD_BOARD:
+      return { ...state, boards: [...state.boards, action.board] }
+    case UPDATE_BOARD:
+      boards = state.boards.map((board) =>
+        board._id === action.board._id ? action.board : board
+      )
+      return { ...state, boards }
+    case ADD_BOARD_MSG:
+      return {
+        ...state,
+        board: {
+          ...state.board,
+          msgs: [...(state.board.msgs || []), action.msg],
+        },
+      }
+    case ADD_GROUP:
+      boards = state.boards.map((board) =>
+        board._id === action.payload.boardId
+          ? {
+              ...board,
+              groups: [...board.groups, action.payload.group],
+            }
+          : board
+      )
+      return { ...state, boards }
+    case UPDATE_GROUP:
+      boards = state.boards.map((board) =>
+        board._id === action.payload.boardId
+          ? {
+              ...board,
+              groups: board.groups.map((group) =>
+                group._id === action.payload.groupId
+                  ? action.payload.group
+                  : group
+              ),
+            }
+          : board
+      )
+      return { ...state, boards }
+    case REMOVE_GROUP:
+      boards = state.boards.map((board) =>
+        board._id === action.payload.boardId
+          ? {
+              ...board,
+              groups: board.groups.filter(
+                (group) => group._id !== action.payload.groupId
+              ),
+            }
+          : board
+      )
+      return { ...state, boards }
 
-        case ADD_TASK:
-            boards = state.boards.map(board =>
-                board._id === action.payload.boardId
-                    ? {
-                          ...board,
-                          groups: board.groups.map(group =>
-                              group._id === action.payload.groupId
-                                  ? {
-                                        ...group,
-                                        tasks: [...group.tasks, action.payload.task],
-                                    }
-                                  : group
-                          ),
-                      }
-                    : board
-            )
-            return { ...state, boards }
-        case UPDATE_TASK:
-            boards = state.boards.map(board =>
-                board._id === action.payload.boardId
-                    ? {
-                          ...board,
-                          groups: board.groups.map(group =>
-                              group._id === action.payload.groupId
-                                  ? {
-                                        ...group,
-                                        tasks: group.tasks.map(task =>
-                                            task._id === action.payload.taskId ? action.payload.task : task
-                                        ),
-                                    }
-                                  : group
-                          ),
-                      }
-                    : board
-            )
-            return { ...state, boards }
-        case REMOVE_TASK:
-            boards = state.boards.map(board =>
-                board._id === action.payload.boardId
-                    ? {
-                          ...board,
-                          groups: board.groups.map(group =>
-                              group._id === action.payload.groupId
-                                  ? {
-                                        ...group,
-                                        tasks: group.tasks.filter(task => task._id !== action.payload.taskId),
-                                    }
-                                  : group
-                          ),
-                      }
-                    : board
-            )
-            return { ...state, boards }
-        default:
-            return newState
-    }
+    case ADD_TASK:
+      boards = state.boards.map((board) =>
+        board._id === action.payload.boardId
+          ? {
+              ...board,
+              groups: board.groups.map((group) =>
+                group._id === action.payload.groupId
+                  ? {
+                      ...group,
+                      tasks: [...group.tasks, action.payload.task],
+                    }
+                  : group
+              ),
+            }
+          : board
+      )
+      return { ...state, boards }
+    case UPDATE_TASK:
+      boards = state.boards.map((board) =>
+        board._id === action.payload.boardId
+          ? {
+              ...board,
+              groups: board.groups.map((group) =>
+                group._id === action.payload.groupId
+                  ? {
+                      ...group,
+                      tasks: group.tasks.map((task) =>
+                        task._id === action.payload.taskId
+                          ? action.payload.task
+                          : task
+                      ),
+                    }
+                  : group
+              ),
+            }
+          : board
+      )
+      return { ...state, boards }
+    case REMOVE_TASK:
+      boards = state.boards.map((board) =>
+        board._id === action.payload.boardId
+          ? {
+              ...board,
+              groups: board.groups.map((group) =>
+                group._id === action.payload.groupId
+                  ? {
+                      ...group,
+                      tasks: group.tasks.filter(
+                        (task) => task._id !== action.payload.taskId
+                      ),
+                    }
+                  : group
+              ),
+            }
+          : board
+      )
+      return { ...state, boards }
+    default:
+      return newState
+  }
 }
