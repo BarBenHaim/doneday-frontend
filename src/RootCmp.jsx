385e518c10a05b40b21921d654cd07d5d988ad96
Ariella Melnikov <melnikov.ariella@gmail.com>
2024-07-18 18:58:30 +0300

    delete appfooter and extra Route to BoardIndex

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -12,7 +12,6 @@ import { AdminIndex } from './pages/AdminIndex.jsx'
 import { UserDetails } from './pages/UserDetails'
 
 import { AppHeader } from './cmps/AppHeader'
-import { AppFooter } from './cmps/AppFooter'
 import { UserMsg } from './cmps/UserMsg.jsx'
 import { LoginSignup } from './pages/LoginSignup.jsx'
 import { Login } from './pages/Login.jsx'
@@ -31,7 +30,6 @@ export function RootCmp() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/about' element={<AboutUs />} />
-        <Route path='board' element={<BoardIndex />} />
         <Route path='board' element={<BoardIndex />} />
         {/* <Route path='board/details' element={<BoardDetails />} /> */}
         <Route path='board/:boardId' element={<BoardDetails />} />
