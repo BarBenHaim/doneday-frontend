bcb3b4876531088ce9cfd78550080bcedbc6b2e9
Ariella Melnikov <melnikov.ariella@gmail.com>
2024-07-18 16:52:54 +0300

    fix rootcmp

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -8,7 +8,7 @@ import { ReviewIndex } from './pages/ReviewIndex.jsx'
 import { ChatApp } from './pages/Chat.jsx'
 import { AdminIndex } from './pages/AdminIndex.jsx'
 
-import { CarDetails } from './pages/CarDetails'
+// import { CarDetails } from './pages/CarDetails'
 import { UserDetails } from './pages/UserDetails'
 
 import { AppHeader } from './cmps/AppHeader'
@@ -27,25 +27,23 @@ export function RootCmp() {
       <AppHeader />
       <UserMsg />
 
-      <div className="first-level">
-    
-          
       <Sidebar />
-         
-
-            <Route path='board' element={<BoardIndex />} />
-            {/* <Route path='board/details' element={<BoardDetails />} /> */}
-            <Route path='board/:boardId' element={<BoardDetails />} />
-            <Route path='user/:id' element={<UserDetails />} />
-            <Route path='review' element={<ReviewIndex />} />
-            <Route path='chat' element={<ChatApp />} />
-            <Route path='admin' element={<AdminIndex />} />
-            <Route path='login' element={<LoginSignup />}>
-              <Route index element={<Login />} />
-              <Route path='signup' element={<Signup />} />
-            </Route>
-          </Routes>
-      </div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<AboutUs />} />
+        <Route path='board' element={<BoardIndex />} />
+        <Route path='board' element={<BoardIndex />} />
+        {/* <Route path='board/details' element={<BoardDetails />} /> */}
+        <Route path='board/:boardId' element={<BoardDetails />} />
+        <Route path='user/:id' element={<UserDetails />} />
+        <Route path='review' element={<ReviewIndex />} />
+        <Route path='chat' element={<ChatApp />} />
+        <Route path='admin' element={<AdminIndex />} />
+        <Route path='login' element={<LoginSignup />}>
+          <Route index element={<Login />} />
+          <Route path='signup' element={<Signup />} />
+        </Route>
+      </Routes>
       {/* <AppFooter /> */}
     </div>
   )
