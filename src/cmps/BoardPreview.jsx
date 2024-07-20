ccba21520a4b759c9bb89aa02dbdbdfd2aaa623b
Ariella Melnikov <melnikov.ariella@gmail.com>
2024-07-20 20:36:50 +0300

    fix bug in starred btn and improve scss

--- src/cmps/BoardPreview.jsx
+++ src/cmps/BoardPreview.jsx
@@ -9,42 +9,43 @@ import { Icon } from 'monday-ui-react-core'
 import logoImg from '../assets/img/logo/only-logo.png'
 
 export function BoardPreview({ board }) {
-  async function handleToggleStarred(event) {
-    event.stopPropagation()
-    toggleStarredBoard(board._id)
-    board.isStarred = !board.isStarred
-  }
 
-  return (
-    <article className='board-preview-container'>
-    <div className='board-preview-content '>
-      <Link to={`/board/${board._id}`} className='board-preview-link'>
-        <div className='board-preview-img'>
-          <img src={boardPreviewImg} alt='boardPreviewImg' />
-        </div>
-        <div className='board-preview-item-wrapper flex'>
-          <Board size="18" label='Expand list' />
-          <div className='board-preview-title'>{board.title}</div>
-        </div>
-      </Link>
-      <button className='starred-btn' title='Starred' onClick={handleToggleStarred}>   
+    async function handleToggleStarred(event) {
+      event.preventDefault()
+      console.log('event',event)
+        // event.stopPropagation()
+        toggleStarredBoard(board._id)
+        board.isStarred = !board.isStarred
+    }
 
-        {board.isStarred ? (
-          // <FontAwesomeIcon icon={faStar} className='fa-star' style={{ color: '#FFD43B' }} title='Favorite' />
-          <Icon iconType={Icon.type.ICON_FONT} icon="fa fa-star" className='yellow-star-icon'/>
-        ) : (
-          <Favorite className='monday-icon' />
-        )}
-           </button>
-    </div>
-    <Link to={`/board/${board._id}`}>
-      <div className='board-preview-navigation'>
-        <img src={logoImg} alt="" />
-        <p>
-          work management {'>'} Main workspace {'>'} {board.title}{' '}
-        </p>
-      </div>
-    </Link>
-  </article>
-);
+    return (
+        <article className='board-preview-container'>
+                <Link to={`/board/${board._id}`} className='board-preview-link'>
+                    <div className='board-preview-img'>
+                        <img src={boardPreviewImg} alt='boardPreviewImg' />
+                    </div>
+                    <div className='board-preview-item-wrapper flex'>
+                    <div className='board-preview-title-wrapper'>
+                        <Board size='18' label='Expand list' />
+                        <span className='board-preview-title'>{board.title}</span>
+                        </div>
+
+                        <button className='starred-btn' title='Starred' onClick={handleToggleStarred}>
+                            {board.isStarred ? (
+                                // <FontAwesomeIcon icon={faStar} className='fa-star' style={{ color: '#FFD43B' }} title='Favorite' />
+                                <Icon iconType={Icon.type.ICON_FONT} icon='fa fa-star' className='yellow-star-icon' />
+                            ) : (
+                                <Favorite className='monday-icon' />
+                            )}
+                        </button>
+                    </div>
+                    <div className='board-preview-navigation'>
+                        <img src={logoImg} alt='' />
+                        <p>
+                            work management {'>'} Main workspace {'>'} {board.title}{' '}
+                        </p>
+                    </div>
+                </Link>
+        </article>
+    )
 }
