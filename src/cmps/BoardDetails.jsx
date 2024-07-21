21ca02d1aa94c0ec19d65da194d976e09a34ec41
Ariella Melnikov <melnikov.ariella@gmail.com>
2024-07-21 19:25:35 +0300

    add headline to board details header

--- src/cmps/BoardDetails.jsx
+++ src/cmps/BoardDetails.jsx
@@ -6,35 +6,38 @@ import { GroupFilter } from './group/GroupsFilter/GroupFilter'
 import { useSelector } from 'react-redux'
 
 export function BoardDetails() {
-  const { boardId } = useParams()
+    const { boardId } = useParams()
 
-  const currBoard = useSelector((storeState) =>
-    storeState.boardModule.boards.find((board) => board._id === boardId)
-  )
-  const [boardsToDisplay, setBoardsToDisplay] = useState(
-    currBoard?.groups || []
-  )
-  useEffect(() => {
-    setBoardsToDisplay(currBoard?.groups || [])
-  }, [currBoard])
+    const currBoard = useSelector((storeState) => storeState.boardModule.boards.find((board) => board._id === boardId))
+    const [boardsToDisplay, setBoardsToDisplay] = useState(currBoard?.groups || [])
+    useEffect(() => {
+        setBoardsToDisplay(currBoard?.groups || [])
+    }, [currBoard])
 
-  useEffect(() =>
-    // loadBoardById(boardId)
-    {}, [boardId])
+    useEffect(() =>
+        // loadBoardById(boardId)
+        {}, [boardId])
 
-  const setFilterBy = (arr) => {
-    setBoardsToDisplay(arr)
-  }
+    const setFilterBy = (arr) => {
+        setBoardsToDisplay(arr)
+    }
 
-  async function loadBoardById(id) {
-    try {
-    } catch {}
-  }
+    async function loadBoardById(id) {
+        try {
+        } catch {}
+    }
 
-  return (
-    <section className="board-details">
-      <GroupFilter setFilterBy={setFilterBy} />
-      <GroupList boardsToDisplay={boardsToDisplay} />
-    </section>
-  )
+    return (
+        <section className='board-details'>
+            <header className='board-details-header'>
+                <div className='board-details-title'>
+                    <h1>{currBoard.title}</h1>
+                </div>
+                <div>
+                    <GroupFilter setFilterBy={setFilterBy} />
+                </div>
+            </header>
+            <GroupList boardsToDisplay={boardsToDisplay} />
+        </section>
+    )
 }
