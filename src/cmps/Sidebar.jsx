0c907a20154044c18fe8eaab588ab9e456e76962
Nir Fakiro <fakironir@gmail.com>
2024-07-22 12:08:10 +0300

    Add icons

--- src/cmps/Sidebar.jsx
+++ src/cmps/Sidebar.jsx
@@ -5,88 +5,102 @@ import { useState } from 'react'
 
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 import {
-    MenuItem,
-    Menu,
-    MenuDivider,
-    DialogContentContainer,
-    MenuButton,
-    SplitButton,
-    SplitButtonMenu,
-    MenuTitle,
+  MenuItem,
+  Menu,
+  MenuDivider,
+  DialogContentContainer,
+  MenuButton,
+  SplitButton,
+  SplitButtonMenu,
+  MenuTitle,
 } from 'monday-ui-react-core'
 import {
-    Favorite,
-    Home,
-    NavigationChevronDown,
-    NavigationChevronUp,
-    DropdownChevronDown,
-    Board,
-    Add,
+  Favorite,
+  Home,
+  NavigationChevronDown,
+  NavigationChevronUp,
+  DropdownChevronDown,
+  Board,
+  Add,
 } from 'monday-ui-react-core/icons'
 
 import { AddBoard } from './AddBoard'
 
 export function Sidebar() {
-    const navigate = useNavigate()
-    const [isCollapsed, setIsCollapsed] = useState(false)
-    const [isAddBoardModalOpen, setIsAddBoardModalOpen] = useState(false)
-    const boards = useSelector((storeState) => storeState.boardModule.boards)
-    const starredBoards = boards.filter((board) => board.isStarred)
+  const navigate = useNavigate()
+  const [isCollapsed, setIsCollapsed] = useState(false)
+  const [isAddBoardModalOpen, setIsAddBoardModalOpen] = useState(false)
+  const boards = useSelector((storeState) => storeState.boardModule.boards)
+  const starredBoards = boards.filter((board) => board.isStarred)
 
-    function handleOnClick(route) {
-        navigate(route)
-    }
+  function handleOnClick(route) {
+    navigate(route)
+  }
 
-    function toggleCollapse() {
-        setIsCollapsed(!isCollapsed)
-    }
+  function toggleCollapse() {
+    setIsCollapsed(!isCollapsed)
+  }
 
-    function toggleAddBoard() {
-        setIsAddBoardModalOpen(!isAddBoardModalOpen)
-    }
+  function toggleAddBoard() {
+    setIsAddBoardModalOpen(!isAddBoardModalOpen)
+  }
 
-    return (
-        <div className='sidebar main-container'>
-            <DialogContentContainer>
-                <Menu>
-                    <MenuItem icon={Home} title='Home' onClick={() => handleOnClick('/board')} />
-                    <MenuDivider />
-                    <MenuItem
-                        icon={Favorite}
-                        title='Favorite'
-                        onClick={() => toggleCollapse()}
-                        rightIcon={
-                            isCollapsed ? <NavigationChevronDown icon={DropdownChevronDown} /> : <NavigationChevronUp />
-                        }
-                    />
-                    {!isCollapsed &&
-                        starredBoards.map((board) => (
-                            <MenuItem
-                                key={board._id}
-                                title={board.title}
-                                onClick={() => handleOnClick(`/board/${board._id}`)}
-                            />
-                        ))}
-                    <MenuDivider />
+  return (
+    <div className="sidebar main-container">
+      <DialogContentContainer>
+        <Menu>
+          <MenuItem
+            icon={Home}
+            title="Home"
+            onClick={() => handleOnClick('/board')}
+          />
+          <MenuDivider />
+          <MenuItem
+            icon={Favorite}
+            title="Favorite"
+            onClick={() => toggleCollapse()}
+            rightIcon={
+              isCollapsed ? (
+                <NavigationChevronDown icon={DropdownChevronDown} />
+              ) : (
+                <NavigationChevronUp />
+              )
+            }
+          />
+          {!isCollapsed &&
+            starredBoards.map((board) => (
+              <MenuItem
+                key={board._id}
+                title={board.title}
+                onClick={() => handleOnClick(`/board/${board._id}`)}
+              />
+            ))}
+          <MenuDivider />
 
-                    <MenuTitle caption='Main workspace' captionPosition='top' />
-                    
-                    <MenuItem
-                        icon={Add}
-                        className='add-board-menu'
-                        tooltipPosition='top'
-                        tooltipContent='Add item to workspace'
-                    />
-                    <MenuTitle caption='Add new' />
-                    <MenuItem icon={Board} title='Board' splitMenuItem>
-                        <Menu>
-                            <MenuItem icon={Board} title='New Board' onClick={toggleAddBoard} />
-                        </Menu>
-                    </MenuItem>
-                </Menu>
-            </DialogContentContainer>
+          <MenuTitle caption="Main workspace" captionPosition="top" />
 
-            {isAddBoardModalOpen && <AddBoard isOpen={isAddBoardModalOpen} onClose={toggleAddBoard} />}
-        </div>
-    )
+          <MenuItem
+            icon={Add}
+            className="add-board-menu"
+            tooltipPosition="top"
+            tooltipContent="Add item to workspace"
+          />
+          <MenuTitle caption="Add new" />
+          <MenuItem icon={Board} title="Board" splitMenuItem>
+            <Menu>
+              <MenuItem
+                icon={Board}
+                title="New Board"
+                onClick={toggleAddBoard}
+              />
+            </Menu>
+          </MenuItem>
+        </Menu>
+      </DialogContentContainer>
+
+      {isAddBoardModalOpen && (
+        <AddBoard isOpen={isAddBoardModalOpen} onClose={toggleAddBoard} />
+      )}
+    </div>
+  )
 }
