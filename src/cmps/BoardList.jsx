6394cc222710dcc961b8df0238827c2e5238a57d
Ariella Melnikov <melnikov.ariella@gmail.com>
2024-07-25 13:21:46 +0300

    remove board list slice

--- src/cmps/BoardList.jsx
+++ src/cmps/BoardList.jsx
@@ -9,7 +9,7 @@ export function BoardList({ boards}) {
             <div
                  className="board-list-container"
             >
-                {boards.slice(0, 4).map(board => (
+                {boards.map(board => (
                     <div key={board._id} className="board-preview-wrapper">
                         <BoardPreview board={board} />
                         </div>
