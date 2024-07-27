2ef70aaae36ca94146d2f18808c1c27b578e72a9
Nir Fakiro <fakironir@gmail.com>
2024-07-27 21:18:23 +0300

    Add activity to the store

--- src/cmps/group/task/dynamicCmps/Comments/ActivityLog.jsx
+++ src/cmps/group/task/dynamicCmps/Comments/ActivityLog.jsx
@@ -1,5 +1,26 @@
-export function ActivityLog({ task, field, value }) {}
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router'
 
-export function addActivty(task, field, value) {
-  console.log(task, field, value)
+export function ActivityLog() {
+  const { boardId } = useParams()
+  const currBoard = useSelector((storeState) =>
+    storeState.boardModule.boards.find((board) => board._id === boardId)
+  )
+  const groups = currBoard.groups || []
+  const tasks = groups.flatMap((group) => group.tasks || [])
+  const activities = currBoard.activities
+  return (
+    <sectio>
+      <ul>
+        {activities.map((active) => (
+          <li>
+            <span className="active-time">
+              {moment(active.createdAt).fromNow()}
+            </span>
+            <p>{ta}</p>
+          </li>
+        ))}
+      </ul>
+    </sectio>
+  )
 }
