2dcdbd5962feb55d7b8cb3b6d50daf5b7b5bf545
Nir Fakiro <fakironir@gmail.com>
2024-07-28 12:59:34 +0300

    Add filter to the side bar

--- src/cmps/Sidebar.jsx
+++ src/cmps/Sidebar.jsx
@@ -5,120 +5,150 @@ import { useState } from 'react'
 
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 import {
-    MenuItem,
-    Menu,
-    MenuDivider,
-    DialogContentContainer,
-    SplitButton,
-    SplitButtonMenu,
-    MenuTitle,
-    MenuItemButton,
-    Dialog,
-    Button,
-    IconButton,
+  MenuItem,
+  Menu,
+  MenuDivider,
+  DialogContentContainer,
+  SplitButton,
+  SplitButtonMenu,
+  MenuTitle,
+  MenuItemButton,
+  Dialog,
+  Button,
+  IconButton,
 } from 'monday-ui-react-core'
 import {
-    Favorite,
-    Home,
-    NavigationChevronDown,
-    NavigationChevronUp,
-    DropdownChevronDown,
-    Board,
-    Add,
+  Favorite,
+  Home,
+  NavigationChevronDown,
+  NavigationChevronUp,
+  DropdownChevronDown,
+  Board,
+  Add,
 } from 'monday-ui-react-core/icons'
 
 import { AddBoard } from './AddBoard'
 
 export function Sidebar() {
-    const navigate = useNavigate()
-    const [isCollapsed, setIsCollapsed] = useState(false)
-    const [isAddBoardModalOpen, setIsAddBoardModalOpen] = useState(false)
-    const boards = useSelector(storeState => storeState.boardModule.boards)
-    const starredBoards = boards.filter(board => board.isStarred)
-    const boardLabel = boards.filter(board => board.label)
+  const navigate = useNavigate()
+  const [isCollapsed, setIsCollapsed] = useState(false)
+  const [isAddBoardModalOpen, setIsAddBoardModalOpen] = useState(false)
+  const boards = useSelector((storeState) => storeState.boardModule.boards)
+  const starredBoards = boards.filter((board) => board.isStarred)
+  const boardLabel = boards.filter((board) => board.label)
+  const [filterBy, setFilterBy] = useState('')
 
-    function handleOnClick(route) {
-        navigate(route)
-    }
+  const filteredBoardLabel = boardLabel.filter((board) =>
+    board.label.toLowerCase().includes(filterBy.toLowerCase())
+  )
 
-    function toggleCollapse() {
-        setIsCollapsed(!isCollapsed)
-    }
+  function handelChange(e) {
+    setFilterBy(e.target.value)
+  }
+  function handleOnClick(route) {
+    navigate(route)
+  }
 
-    function toggleAddBoard() {
-        setIsAddBoardModalOpen(!isAddBoardModalOpen)
-    }
+  function toggleCollapse() {
+    setIsCollapsed(!isCollapsed)
+  }
 
-    return (
-        <div className='sidebar main-container'>
-            <DialogContentContainer>
+  function toggleAddBoard() {
+    setIsAddBoardModalOpen(!isAddBoardModalOpen)
+  }
+
+  return (
+    <div className="sidebar main-container">
+      <DialogContentContainer>
+        <Menu>
+          <MenuItem
+            icon={Home}
+            title="Home"
+            onClick={() => handleOnClick('/board')}
+          />
+          <MenuDivider />
+          <MenuItem
+            icon={Favorite}
+            title="Favorite"
+            onClick={() => toggleCollapse()}
+            rightIcon={
+              isCollapsed ? (
+                <NavigationChevronDown icon={DropdownChevronDown} />
+              ) : (
+                <NavigationChevronUp />
+              )
+            }
+          />
+          {!isCollapsed &&
+            starredBoards.map((board) => (
+              <MenuItem
+                icon={Board}
+                key={board._id}
+                title={board.title}
+                onClick={() => handleOnClick(`/board/${board._id}`)}
+              />
+            ))}
+          <MenuDivider />
+        </Menu>
+        <div className="add-button-dialog">
+          <Dialog
+            content={
+              <DialogContentContainer>
                 <Menu>
-                    <MenuItem icon={Home} title='Home' onClick={() => handleOnClick('/board')} />
-                    <MenuDivider />
-                    <MenuItem
-                        icon={Favorite}
-                        title='Favorite'
-                        onClick={() => toggleCollapse()}
-                        rightIcon={
-                            isCollapsed ? <NavigationChevronDown icon={DropdownChevronDown} /> : <NavigationChevronUp />
-                        }
-                    />
-                    {!isCollapsed &&
-                        starredBoards.map(board => (
-                            <MenuItem
-                                icon={Board}
-                                key={board._id}
-                                title={board.title}
-                                onClick={() => handleOnClick(`/board/${board._id}`)}
-                            />
-                        ))}
-                    <MenuDivider />
+                  <MenuTitle caption="Add new" />
+                  <MenuItem icon={Board} title="Board" splitMenuItem>
+                    <Menu>
+                      <MenuItem
+                        icon={Board}
+                        title="New Board"
+                        onClick={toggleAddBoard}
+                      />
+                    </Menu>
+                  </MenuItem>
                 </Menu>
-                <div className='add-button-dialog'>
-                    <Dialog
-                        content={
-                            <DialogContentContainer>
-                                <Menu>
-                                    <MenuTitle caption='Add new' />
-                                    <MenuItem icon={Board} title='Board' splitMenuItem>
-                                        <Menu>
-                                            <MenuItem icon={Board} title='New Board' onClick={toggleAddBoard} />
-                                        </Menu>
-                                    </MenuItem>
-                                </Menu>
-                            </DialogContentContainer>
-                        }
-                        hideTrigger={['clickoutside', 'onContentClick']}
-                        isOpen
-                        modifiers={[
-                            {
-                                name: 'preventOverflow',
-                                options: {
-                                    mainAxis: false,
-                                },
-                            },
-                        ]}
-                        position='left-start'
-                        showTrigger={['click']}
-                    >
-                        <IconButton icon={Add} kind='primary' />
-                    </Dialog>
-                </div>
-                <Menu>
-                    <MenuDivider />
-                    <MenuTitle caption='Main workspace' captionPosition='top' />
-                    {boardLabel.map(board => (
-                        <MenuItem
-                            icon={Board}
-                            key={board._id}
-                            title={board.title}
-                            onClick={() => handleOnClick(`/board/${board._id}`)}
-                        />
-                    ))}
-                </Menu>
-            </DialogContentContainer>
+              </DialogContentContainer>
+            }
+            hideTrigger={['clickoutside', 'onContentClick']}
+            isOpen
+            modifiers={[
+              {
+                name: 'preventOverflow',
+                options: {
+                  mainAxis: false,
+                },
+              },
+            ]}
+            position="left-start"
+            showTrigger={['click']}
+          >
+            <input
+              className="search-bar"
+              type="text"
+              placeholder="Search..."
+              value={filterBy}
+              onChange={handelChange}
+            ></input>
 
-            {isAddBoardModalOpen && <AddBoard isOpen={isAddBoardModalOpen} onClose={toggleAddBoard} />}
+            <IconButton icon={Add} kind="primary" />
+          </Dialog>
         </div>
-    )
+        <Menu>
+          <MenuDivider />
+          <MenuTitle caption="Main workspace" captionPosition="top" />
+          {filteredBoardLabel.map((board) => (
+            <MenuItem
+              icon={Board}
+              key={board._id}
+              title={board.title}
+              onClick={() => handleOnClick(`/board/${board._id}`)}
+            />
+          ))}
+        </Menu>
+      </DialogContentContainer>
+
+      {isAddBoardModalOpen && (
+        <AddBoard isOpen={isAddBoardModalOpen} onClose={toggleAddBoard} />
+      )}
+    </div>
+  )
 }
